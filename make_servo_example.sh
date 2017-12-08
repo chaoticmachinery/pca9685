91b7b6aa2842a99b391a851b2642c4548c081e2d
K Murphy <chaoticmachinery@users.noreply.github.com>
2017-12-07 19:13:01 -0500

    Delete make_servo_example.sh

--- make_servo_example.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm servo
-
-g++ servo_example.cpp  -o servo  -L /usr/local/lib -I /usr/local/include -I. -Wall -fPIC -lPCA9685  -Wconversion-null   -O3  -pthread -lpthread -std=c++11 
